57f3ed0402044e25c749a30825d6d7b5845a34dd
nuggiestronk <iambentiden@gmail.com>
2015-05-24 04:44:05 +0200

    sachen

---
+++ Kurvenschar.m
@@ -0,0 +1,57 @@
+## Copyright (C) 2015 MINT
+## 
+## This program is free software; you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or
+## (at your option) any later version.
+## 
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+## -*- texinfo -*- 
+## @deftypefn {Function File} {@var{retval} =} Kurvenschar (@var{input1}, @var{input2})
+##
+## @seealso{}
+## @end deftypefn
+
+## Author: MINT <MINT@MINT-PC>
+## Created: 2015-05-21
+##schar(:,1) = rmse(polyval(schar(i,:),vals),realvals) 
+function [newRmse] = Kurvenschar (best,kids,funcvals,realvals,z,generations)
+
+ord = size(best)(:,2)
+
+for i = 1:ord
+a (:,i) = floor(best(:,i)*10^z)/10^z -1/10^z
+b (:,i) = floor(best(:,i)*10^z)/10^z +1/10^z
+end
+
+
+
+
+
+
+for i = 1:ord
+
+schar(:,i) = rand(1,kids)'.*(b(:,i)-a(:,i)).+a(:,i)         
+end
+
+for i = 1:kids
+schar(i,ord+1) = rmse(polyval(schar(i,:),funcvals),realvals)
+end
+
+bestFilter = (schar(:,ord+1)==min(schar(:,ord+1)))
+
+newBest = (schar(bestFilter,1:ord))
+newRmse = (schar(bestFilter,ord+1))
+
+if (generations != 1) Kurvenschar (newBest,kids,funcvals,realvals,z+1,generations-1)
+
+else newBest = (schar(bestFilter,1:ord))
+endif
+endfunction
---
+++ skript.m
@@ -0,0 +1,94 @@
+nations = dlmread("nations.csv", ",", 1, 0)
+BIPproKopf = (nations(:,4)*1e+6)./nations(:,7)
+Lebenserwartung = nations(:,5)
+plot(BIPproKopf, Lebenserwartung, "ro")
+plot polyval([6000,67], 0:140000)
+plot (polyval([6000,67], 0:140000))
+plot(BIPproKopf, Lebenserwartung, "ro")
+hold on
+plot (polyval([6000,67], 0:140000))
+hold off
+plot(BIPproKopf, Lebenserwartung, "ro")
+plot (polyval([0.0006,67], 0:140000))
+plot(BIPproKopf, Lebenserwartung, "ro")
+hold on
+plot (polyval([0.0006,67], 0:140000))
+plot (polyval([0.00006,67], 0:140000))
+hold off
+hold off
+plot(BIPproKopf, Lebenserwartung, "ro")
+plot (polyval([0.00006,67], 0:140000))
+hold off
+plot(BIPproKopf, Lebenserwartung, "ro")
+hold on
+plot (polyval([0.00006,67], 0:140000))
+plot (polyval([0.00008,67], 0:140000))
+plot (polyval([0.00010,67], 0:140000))
+plot (polyval([0.00012,67], 0:140000))
+plot (polyval([0.00014,67], 0:140000))
+gerade = (polyval([0.00012,67], 0:140000))
+gerade = (polyval([0.00012,67], max(BIPproKopf))
+gerade = (polyval([0.00012,67], 0:max(BIPproKopf))
+max(BIPproKopf)
+gerade = (polyval([0.00012,67], 0:max(BIPproKopf))
+wat
+gerade = (polyval([0.00012,67], 0:(max(BIPproKopf)))
+gerade = (polyval([0.00012,67], 0:(max(BIPproKopf)))
+gerade = (polyval([0.00012,67], 0:140000))
+max = max(BIPproKopf)
+gerade = (polyval([0.00012,67], 0:max))
+gerade = (polyval([0.00012,67], 0:max(BIPproKopf))
+gerade = (polyval([0.00012,67], 0:max(BIPproKopf))
+gerade = (polyval([0.00012,67], 0:max))
+rmse(gerade,lebenserwartung)
+rmse(gerade,Lebenserwartung)
+rmse(gerade,Lebenserwartung)
+rmse(gerade,Lebenserwartung)
+rmse(gerade,Lebenserwartung)
+rmse(gerade,Lebenserwartung)
+rmse(gerade,Lebenserwartung)
+rmse(gerade,Lebenserwartung)
+rmse(gerade,Lebenserwartung)
+rmse(gerade,Lebenserwartung)
+rmse(gerade,Lebenserwartung)
+rmse(gerade,Lebenserwartung)
+rmse(gerade,Lebenserwartung)
+gerade .- lebenserwartung
+gerade .- Lbenserwartung
+gerade .- Lenserwartung
+gerade .- Lebenserwartung
+Sachen = gerade .- Lebenserwartung
+mean(Sachen)
+Mean = mean(Sachen)
+gerade = (polyval([0.00012,67], BIPproKopf))
+rmse(gerade, BIPproKopf)
+gerade
+gerade .- Lebenserwartung
+gerade = (polyval([0.00012,67], BIPproKopf))
+rmse(gerade,Lebenserwartung)
+gerade.-Lebenserwartung
+Sachen = gerade.-Lebenserwartung
+mean(Sachen)
+BIPproKopf .- gerade
+Lebenserwartung
+gerade
+mean(Lebenserwartung .- gerade)
+gerade .- Lebenserwartung
+pow2(gerade .- Lebenserwartung)
+sqrt(pow2(gerade .- Lebenserwartung))
+mean(sqrt(pow2(gerade .- Lebenserwartung)))
+(sqrt(pow2(mean(gerade .- Lebenserwartung))))
+(sqrt(mean(pow2(gerade .- Lebenserwartung))))
+(sqrt(mean(pow2(gerade .- Lebenserwartung))))
+(sqrt(mean(pow2(gerade .- Lebenserwartung))))
+(sqrt(mean(gerade .- Lebenserwartung).^2)))
+(sqrt(mean(gerade .- Lebenserwartung).^2))))
+(sqrt(mean(gerade .- Lebenserwartung).^2))))
+(sqrt(mean((gerade .- Lebenserwartung).^2))))
+(sqrt(mean((gerade .- Lebenserwartung).^2)))
+edit Kurvenschar.m
+rand(1:1000).*(10-5).+5
+rand(1:1000)
+rand(1,1000)
+rand(1,1000).*(10-5).+5
+max(rand(1,1000).*(10-5).+5)
